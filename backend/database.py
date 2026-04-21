7d5d598e2339a0e83f2304c2f0d43c4a1b51fc05
Gopala Kanva <bhupendhragurjar@gmail.com>
2026-04-22 00:55:23 +0530

    Refactor database connection setup and load env vars

--- backend/database.py
+++ backend/database.py
@@ -1,25 +1,16 @@
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 
-# DATABASE CONNECTION
-DATABASE_URL = "mysql+pymysql://root:password@localhost/autosched"
+load_dotenv()
 
-engine = create_engine(
-    DATABASE_URL,
-    pool_pre_ping=True,
-    pool_recycle=280
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
-
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-# DB dependency (FastAPI use karega)
 def get_db():
     db = SessionLocal()
     try:
