96efbf7efb4059b94070e1c2f4d601b173b286f7
Gopala Kanva <bhupendhragurjar@gmail.com>
2026-04-22 14:47:16 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://your-frontend-url"
+        "https://auto-sched-pi.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
