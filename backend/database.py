bafc958391d7fd827c809d265aad652721828454
Gopala Kanva <bhupendhragurjar@gmail.com>
2026-04-26 16:11:23 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -3,8 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-# ✅ Supabase PostgreSQL URL (SSL REQUIRED)
-DATABASE_URL = "postgresql+psycopg2://postgres.yvqggktfexvndiquotlx:Gopala%4020102004@aws-1-ap-south-1.pooler.supabase.com:6543/postgres"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL,
