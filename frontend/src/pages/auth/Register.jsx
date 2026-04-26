f6179e655b77f6cfcbd3400642854538b92d0b81
Gopala Kanva <bhupendhragurjar@gmail.com>
2026-04-26 14:47:23 +0530

    Update Register.jsx

--- frontend/src/pages/auth/Register.jsx
+++ frontend/src/pages/auth/Register.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
-import api from "../../api";
+import api from "../../services/api";
 import {
   EnvelopeIcon,
   LockClosedIcon,
